9d6595f3b2eef05b558e78d32f487c9bcbe58e37
yash bhandari <yashbhandari1093@gmail.com>
2026-09-16 17:19:14 +0530

    Add multithreading and custom exception classes

---
+++ src/EmployeeNotFoundException.java
@@ -0,0 +1,6 @@
+public class EmployeeNotFoundException extends Exception {
+    public EmployeeNotFoundException(String message){
+        super(message);
+    }
+
+}
---
+++ src/EmployeeProcessor.java
@@ -0,0 +1,24 @@
+import java.util.Optional;
+
+public class EmployeeProcessor extends Thread {
+    private EmployeeManager employeeManager;
+    private int employeeId;
+
+    public EmployeeProcessor(EmployeeManager employeeManager, int employeeId){
+        this.employeeManager = employeeManager;
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public void run(){
+        try{
+            Optional<Employee> employee = employeeManager.getEmployeeById(employeeId);
+            employee.ifPresentOrElse(
+                    emp -> System.out.println("Processing Employee:"+ emp),
+                    () -> System.out.println("Employee with ID "+employeeId+" not found.")
+            );
+        }catch(Exception e){
+            System.out.println("Error processing employee Data: "+ e.getMessage());
+        }
+    }
+}
